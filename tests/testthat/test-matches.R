11f407f936308132f696b27bd88bd79fc6ec921d
Max Qiu <ytqiuhaowen@gmail.com>
2022-07-25 15:30:46 -0400

    add tests

---
+++ tests/testthat/test-matches.R
@@ -0,0 +1,15 @@
+test_that("uss_make_matches works", {
+
+  # use the function
+  italy = uss_make_matches(engsoccerdata::italy, "Italy")
+  expect_true(tibble::is_tibble(italy))
+
+  expect_named(italy, c("country", "tier", "season", "date", "home", "visitor",
+                        "goals_home", "goals_visitor"))
+
+  expect_identical(unique(italy$country), "Italy")
+
+  # when finding a bug, add a test
+  expect_s3_class(italy$tier, "factor")
+
+})
