716f41b491e1f7f22d34f3e04b0545d4c9bc5092
Max Qiu <ytqiuhaowen@gmail.com>
2022-07-26 12:13:24 -0400

    add validate

--- R/matches.R
+++ R/matches.R
@@ -10,6 +10,12 @@
 #' @examples
 #' uss_make_matches(engsoccerdata::spain, "Spain")
 uss_make_matches <- function(data_engsoc, country) {
+
+  # validate
+  validate_data_frame(data_engsoc)
+  validate_cols(data_engsoc, cols_engsoc())
+
+  # put into standard form
   result <-
     data_engsoc |>
     tibble::as_tibble() |>
